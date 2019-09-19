717b2eeb975081a695dccef38a143223f6b5c777
Clayton <schneiderc2016@gmail.com>
2019-09-18 22:25:56 -0400

    minor changes, still figuring out how everything works

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,33 +1,85 @@
 <template>
-  <Layout>
-    
-    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
-    
-    <h1>Hello, world!</h1>
-   
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
+  <body>
+    <header class="site-header">
+      <g-image class="logo" src="~/assets/logo.png" height="110px"/>
+      <nav>
+        <g-link to="/">Home</g-link>
+        <g-link to="/about">About</g-link>
+        <g-link to="/blog">Blog</g-link>
+        <g-link to="/contact">Contact</g-link>
+      </nav>
+    </header>
 
-    <p class="home-links">
-      <a href="https://gridsome.org/docs" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
-    </p>
+    <div class="large-hero">
+      <g-image class="large-hero__image" src="~/assets/hero.jpg"/>
+      <div class="large-hero__text-content">
+        <h1 class="large-hero--title">Haley Pesce</h1>
+        <h2 class="large-hero--typewriter">Traveler</h2>
+        <h2 class="large-hero--typewriter">Food Enthusiast</h2>
+        <h2 class="large-hero--typewriter">Lifestyle</h2>
+      </div>
+    </div>
 
-  </Layout>
+    <div class="blog">
+      <h2>Follow my recent adventures</h2>
+
+      <div class="blog__post">
+        <h3>Title</h3>
+        <h4>Date</h4>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus fugiat facilis vel autem quam porro eum quis error inventore quaerat?</p>
+        <h5>Read More >></h5>
+      </div>
+      
+      <button>More</button>
+    </div>
+
+    <div class="instagram">
+      <div class="instagram__row">
+        <g-image/>
+        <g-image/>
+        <g-image/>
+        <g-image/>
+      </div>
+
+
+    </div>
+    
+  </body>
+
+
+
+
 </template>
 
 <script>
 export default {
   metaInfo: {
-    title: 'Hello, world!'
+    title: 'Haley Pesce - Lifestyle Blog!'
   }
 }
 </script>
 
-<style>
-.home-links a {
-  margin-right: 1rem;
+<style lang="scss" scoped>
+.site-header {
+  position: absolute;
+  z-index: 2;
 }
+
+.large-hero {
+  position: relative;
+
+  &__image {
+    width: 100%;
+    height: 100vh;
+    object-fit: cover;
+  }
+
+  &__text-content {
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+}
+
+
 </style>
