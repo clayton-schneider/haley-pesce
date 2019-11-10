4e67054617a930e47ab2cd741ed7484bfa1287f7
Clayton <schneiderc2016@gmail.com>
2019-11-10 17:28:33 -0500

    Tried fixing form

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -7,7 +7,7 @@
       </div>
       <div class="right">
         <h1 class="right__headline">Contact Me!</h1>
-        <form class="form" name="contant" method="POST" data-netlify="true">
+        <form class="form" name="contact" method="POST" data-netlify="true" action="/contact">
           <div class="form__name">
             <label for="form-field-name" class="form__label">Name</label>
             <input size="1" type="text" name="form_fields[name]" id="form-field-name" class="form__input">
